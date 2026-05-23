c19f853dd1de7c46468e4c7b54c8f72c2a1eba5f
EvaGut <evag8928@gmail.com>
2026-05-22 22:13:54 -0700

    created the very basic log in form and table to show the book lists and started to work on search form

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 import './style/css/index.css';
-
+import Form from './components/Log_In_Form.jsx';
+import Table from './components/List.jsx';
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<App />
+		<Table />
 	</StrictMode>
 );
